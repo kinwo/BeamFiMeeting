3ae9950f09d5ac7cc5ccd7a6ede6ceaa97d49537
Henry Chan <henry@kinwo.net>
2023-03-10 15:29:10 +1000

    [CON-7 Zoom] Add meetingNumber

--- apps/api/pages/api/signature.ts
+++ apps/api/pages/api/signature.ts
@@ -27,7 +27,9 @@ export default async function handler(
   const isValid = isInteger(meetingNumber) && isPositive(meetingNumber)
 
   if (!isValid) {
-    res.status(400).send("Invalid request body parameter meetingNumber")
+    res
+      .status(400)
+      .send(`Invalid request body parameter meetingNumber: ${meetingNumber}`)
     return
   }
 
